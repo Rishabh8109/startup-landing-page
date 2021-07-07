3d4f2d24e8d5bc4752558046e6103c1492c5b504
rishabh vishwakarma <vishwakarmarishabh378@gmail.com>
2021-07-07 04:31:23 -0700

    intial commit

--- components/Pricing.js
+++ components/Pricing.js
@@ -1,15 +1,50 @@
-import styles from '../styles/Price.module.css';
-import featureStyles from '../styles/Feature.module.css';
-import PricingTab from './Tabs/PricingTab';
+import styles from "../styles/Price.module.css";
+import featureStyles from "../styles/Feature.module.css";
+import PricingTab from "./Tabs/PricingTab";
+import Image from "next/image";
+import { members } from "../contants/members";
+import {FaFacebookF} from 'react-icons/fa';
+import {AiOutlineTwitter} from 'react-icons/ai';
+import {AiOutlineInstagram} from 'react-icons/ai';
 
 function Pricing() {
-    return (
-        <div className={styles.container}>
-            <p className={featureStyles.title}>Pricing Plan</p>
-            <h2 className={featureStyles.subtitle}>Choose Your Pricing Policy</h2>
-            <PricingTab />
+  return (
+    <div className={styles.container}>
+      <p className={featureStyles.title}>Pricing Plan</p>
+      <h2 className={featureStyles.subtitle}>Choose Your Pricing Policy</h2>
+      <PricingTab />
+      <div className={styles.team}>
+        <p className={featureStyles.title}>Pricing Plan</p>
+        <h2 className={featureStyles.subtitle}>
+          The most qualified and talented individuals
+        </h2>
+        <div className={styles.members}>
+          {members.map(({ img_url, name, post }) => (
+            <>
+              <div className={styles.member_card}>
+                <img src={img_url} alt="members" />
+                <div className={styles.member_data}>
+                  <h4>{name}</h4>
+                  <p>{post}</p>
+                </div>
+                <div className={styles.social_media_account}>
+                   <p>
+                     <FaFacebookF />
+                   </p>
+                    <p>
+                     <AiOutlineTwitter />
+                   </p> 
+                    <p>
+                     <AiOutlineInstagram />
+                   </p>
+                </div>
+              </div>
+            </>
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Pricing;
---
+++ contants/members.js
@@ -0,0 +1,32 @@
+export const members = [
+    {
+       name : "Saimon Harmor",
+       post : "CEO and founder",
+       img_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvBfYOFZeVd-vB6uCNlmjna1zcgd0_JYrij9NP1To1ZWpyBxhmpVjAnJ2mdYiJ7J13Gso&usqp=CAU"
+    },
+    {
+       name : "Aren Nunez",
+       post : "Founder",
+       img_url :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyW1v_mM9vuI9Sd7amcifLxqjlFN7bXlvv3LLtPHuLm1khSZwqsovb7bbOWb-z5CRwqnU&usqp=CAU"
+    },
+    {
+       name : "Saimon",
+       post : "Web Designer",
+       img_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE8EfwJr3oTkP_iCisO63afTY7BZLbzVFxeVHVGE5gc2BtEjZsCgyhitu8FmdEAi_cSbo&usqp=CAU"
+    },
+    {
+       name : "Line jutelia",
+       post : "Web Developer",
+       img_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHBEzK9i-neyHxGRgLjHUrNQacF6o734ltVVIVs_85lF6vEYnJIykUdxXkukmUYzDjf0&usqp=CAU"
+    },
+    {
+       name : "Saimon Harmor",
+       post : "CEO and founder",
+       img_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt-JmDfLz7ErRiTZ9vIme55A9JGQqdx8qJ_xQ_lB2UIqGAFELpsKQQ8xuTSrlqrly-tSQ&usqp=CAU"
+    },
+    {
+       name : "Harmor",
+       post : "CTO and founder",
+       img_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQTea5qn0pcxaH20U9Nyv7UbOpPrZ3VSkTgt_LcM-m1avpBEF7tMEDVUHFLDlZwRGAfF8&usqp=CAU"
+    },
+]
